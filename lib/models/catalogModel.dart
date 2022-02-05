5b4feed0c7f8e301ba527fdcc5f36453244bc663
NoobDeveloper412 <mzuhair412@gmail.com>
2022-02-05 14:10:49 +0500

    Created the catalogModel.

---
+++ lib/models/catalogModel.dart
@@ -0,0 +1,27 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+      id: "000000001",
+      name: "iPhone 12 Pro",
+      desc: "This is the 12th iPhone in the apple iPhone series.",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+];
